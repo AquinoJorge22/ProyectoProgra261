c5ae669919f3a2b732da7e722d52aefaf7fe220f
dianeYCS <dycs@ciencias.unam.mx>
2025-12-04 22:54:03 -0600

    Update TAREAS.java

--- TAREAS.java
+++ TAREAS.java
@@ -4,181 +4,169 @@
  * @author Blancas Mejía Laura Mariana
  * @author Campos Sierra Diane Yriatzi
  * @author Eugenio López Maritza Marlem 
- * @version 1.0, 23/05/2025
+ * @version 1.0, 
  * @see TAREAS
  */
 import java.time.LocalDate;
 
 public class TAREAS {
-
-    public String id;
-    public String estado;
-    public USUARIO usuario;
-    public String descripcion;
+   
+    // Atriburtos principales
+    public String id; //id unico de la tarea
+    public String estado; // "pendiente", "enCurso","completada"
+    public USUARIO usuario; // Usuario, propietario de la tarea
+    public String descripcion; //Texto con la descripcion de la tarea 
+   
+    // Fechas relevantes
     public LocalDate fechaEstimadaInicio;
     public LocalDate fechaInicio;
     public LocalDate fechaEstimadaFin;
     public LocalDate fechaFin;
 
-    public TAREAS(String id, String estado, USUARIO usuario, String descripcion, LocalDate fechaEstimadaInicio,
-            LocalDate fechaInicio, LocalDate fechaEstimadaFin, LocalDate fechaFin) {
+
+
+    /**
+     * Construtor de la clase TAREAS.
+     *
+     * Se incluye @throws para indicar que el constructor puede generar
+     * una excepción si se recibe información inválida (por ejemplo, fechas en orden incorrecto).
+     *
+     * @param id Identificador único de la tarea.
+     * @param descripcion Breve dictamen de lo que se debe realizar.
+     * @param usuario Usuario al que se le asigno a la tarea (puede ser null si no hay aún uno asignado).
+     * @param fechaEstimadaInicio Fecha estimada de inicio. No puede ser previa a la fecha actual.
+     * @param fechaEstimadaFin Fecha estimada en la que debería finalizar la tarea.
+     *
+     * @throws IllegalArgumentException si las fechas estimadas no cumplen reglas lógicas.
+     */
+
+    public TAREAS(String id, String descripcion, USUARIO usuario, 
+        LocalDate fechaEstimadaInicio, LocalDate fechaEstimadaFin) {
+        
+
+        if (fechaEstimadaInicio == null || fechaEstimadaFin == null){
+            throw new IllegalArgumentException("Las fechas estimadas no pueden ser nulas :).");
+        }    
+         if (fechaEstimadaInicio.isBefore(LocalDate.now())){
+            throw new IllegalArgumentException("Las fechas estimada de inicio no puede ser anterior a la fecha actual. ");
+        }   
+         if (fechaEstimadaFin.isBefore(LocalDate.now())){
+             throw new IllegalArgumentException("La fecha estimada final no puede ser antes de la fecha estimada de inicio.");
+        }   
+
+
         this.id = id;
-        this.estado = estado;
-        this.usuario = usuario;
         this.descripcion = descripcion;
+        this.usuario = usuario;
+        this.estado = "pendiente";
+       
         this.fechaEstimadaInicio = fechaEstimadaInicio;
-        this.fechaInicio = fechaInicio;
         this.fechaEstimadaFin = fechaEstimadaFin;
-        this.fechaFin = fechaFin;
+
+        this.fechaInicio = null;
+        this.fechaFin = null;
     }
 
     /**
-     * Muestra la forma del usuario.
+     * Muestra la informacion resumida de la tarea.
      */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
+
         sb.append("ID: ").append(id).append("\n");
         sb.append("Descripción: ").append(descripcion).append("\n");
         sb.append("Estado: ").append(estado).append("\n");
-
-    if ("pendiente".equals(estado) || "en curso".equals(estado) || "completada".equals(estado)) {
+        sb.append("Asignado a : ").append(usuario != null ? usuario.getNickname() : "Sin asignacion").append("\n");
         sb.append("Fecha estimada de inicio: ").append(fechaEstimadaInicio).append("\n");
-        sb.append("Fecha estimada de finalización: ").append(fechaEstimadaFin).append("\n");
-    }
-
-    if ("en curso".equals(estado) || "completada".equals(estado)) {
-        sb.append("Fecha de inicio: ").append(fechaInicio).append("\n");
-    }
-
-    if ("completada".equals(estado)) {
-        sb.append("Fecha de finalización: ").append(fechaFin).append("\n");
-    }
-
-    sb.append("Propietario: ").append(usuario != null ? usuario.getNickname() : "No hay Información");
-
-    return sb.toString();
+        sb.append("Fecha estimada final: ").append(fechaEstimadaFin).append("\n");
+    
+    
+      if (fechaInicio != null) sb.append("Fecha real de inicio: ").append(fechaInicio).append("\n");
+      if (fechaFin != null) sb.append("Fecha final real: ").append(fechaFin).append("\n");
+    
+    
+      return sb.toString();
     
     } 
 
-    //Getters
-    public USUARIO getUsuario(){
-        return usuario;
+
+    /**
+     * Cambio del estado de la tarea
+     * Se esta respetando el que la tarea pase por:
+     * pendiente - enCurso - completada.
+     */
+
+    public void setEstado(String nuevoEstado){
+        if (nuevoEstado == null){
+            throw new IllegalArgumentException("El estado no puede ser nulo".);
+        }
+
+        String estadoNormalizado = nuevoEstado.toLowerCase().replace(" ","");
+
+        switch (estadoNormalizado){
+            case "pendiente":
+                this.estado = "pendinete";
+                break;
+            case "encurso": 
+              if (!estado.equalsIgnoreCase("pendiente")){
+                throw new IllegalArgumentException("Solo se puede pasar a 'En curso' desde 'Pendiente'".);
+              }
+
+              this.estado = "enCurso";
+              this.fechaInicio = LocalDate.now();
+              break;
+
+            case "completada":
+                if (!estado.equalsIgnoreCase("encurso")){
+                 throw new  IllegalArgumentException("Solo se puede completar una tarea que ya estaba 'En curso'");
+                }  
+                this.estado ="completada";
+                this.fechaFin = LocalDate.now();
+                break;
+
+            default:
+                throw new IllegalArgumentException("Estado no reconocido: " + nuevoEstado);
+
+        }
     }
-    public String getId() {
+
+    // --------- GETTERS -------
+    public String get Id(){
         return id;
     }
-
-    public String getEstado() {
-        return estado;
+    public String getEstado(){ 
+        return estado; 
     }
-
-    public String getDescripcion() {
-        return descripcion;
+    public String getDescripcion(){ 
+        return descripcion; 
     }
-
+    public USUARIO getUsuario(){
+         return usuario; 
+        }
     public LocalDate getFechaEstimadaInicio() {
-        return fechaEstimadaInicio;
+         return fechaEstimadaInicio;
+        }
+    public LocalDate getFechaInicio(){
+        return  fechaInicio;
     }
-
-    public LocalDate getFechaInicio() {
-        return fechaInicio;
-    }
-
-    public LocalDate getFechaEstimadaFin() {
+    public LocalDate getFechaEstimadaFin(){
         return fechaEstimadaFin;
     }
-
-    public LocalDate getFechaFin() {
-        return fechaFin;
+    public Local Date getFechaFin(){
+        return fechafin;
     }
 
-    //Setters
-    /**
-     * Asigna un ID a cada tarea
-     * @param id ID único
-     */
-    public void setId(String id) {
-        this.id = id;
+    // ------SETTERS -------
+    public void setUsuario (USUARIO usuario){
+        this.usuario = usuario;
     }
-
-    /**
-     * Asigna una descripción a la tarea
-     * @param descripcion en texto de cada tarea
-     */
-
-    public void setDescripcion(String descripcion) {
+    public void setDescripcion(String descripcion){
         this.descripcion = descripcion;
     }
-
-    //verifica el estado
-    /**
-     * Asigna la fecha estimada de inicio en caso de que el estado sea "pendiente"
-     * @param fecha estimada de inicio
-     */
-    public void setFechaEstimadaInicio(LocalDate fechaEstimadaInicio) {
-        if ("pendiente".equals(this.estado)) {
-            this.fechaEstimadaInicio = fechaEstimadaInicio;
-        }
-    }
-
-    /**
-     * Asigna la fecha estimada de finalización  si el estado es "pendiente"
-     * @param fechaEstimadaFin Fecha estimada de finalización
-     */
-     public void setFechaEstimadaFin(LocalDate fechaEstimadaFin) {
-        if ("pendiente".equals(this.estado)) {
-            this.fechaEstimadaFin = fechaEstimadaFin;
-        }
-    }
+    
 
 
-    //setter de estados para fecha de inicio y final
-    /**
-     * Cambia el estado de la tarea
-     * @param  Nuevo estado de la tarea
-     */
-    public void setEstado(String nuevoEstado) {
-        if (nuevoEstado == null) return;
-
-        if (nuevoEstado.equals("pendiente")) {
-            if (this.estado == null) {
-                this.estado = "pendiente";
-            }
-
-        } else if (nuevoEstado.equalsIgnoreCase("enCurso")) {
-            if ("pendiente".equals(this.estado)) {
-                this.estado = "enCurso";
-                if (this.fechaInicio == null) {
-                    this.fechaInicio = LocalDate.now();
-                }
-            }
-
-        } else if (nuevoEstado.equalsIgnoreCase("completada")) {
-            if ("enCurso".equals(this.estado)) {
-                this.estado = "completada";
-                if (this.fechaFin == null) {
-                    this.fechaFin = LocalDate.now();
-                }
-            }
-
-        } else {
-            throw new IllegalArgumentException("Estado inválido");
-        }
-    }
-
-
-public void setUsuario(USUARIO asignado) {
-    this.usuario = usuario;
 }
-}
-
-
-
-//EJEMPLO LOCAL DATE
-/*
-LocalDate cumple = LocalDate.of(2000, 5, 4); // 4 de mayo del 2000
- */
-
 
 
